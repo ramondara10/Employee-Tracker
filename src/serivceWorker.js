afc419244514483a04748c92eeb39ec1cc103dbd
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2021-06-23 20:43:11 -0500

    time to test the app

--- src/serivceWorker.js
+++ src/serivceWorker.js
@@ -1,3 +1,4 @@
+//the service worker is optional it can make the app a little faster 
 const isLocalhost = Boolean(
     window.location.hostname === 'localhost' ||
       // [::1] is the IPv6 localhost address.
